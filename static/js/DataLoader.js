5c38d91e368b1c6fc315aa42e00ab3825054a617
mankamolnar <markmanomolnar@gmail.com>
2017-03-21 14:57:12 +0100

    wrong json format fixed

--- static/js/DataLoader.js
+++ static/js/DataLoader.js
@@ -1,19 +1,19 @@
 function LocalStorageState() {
 
     this.get_all_boards = function() {
-        return "[{id:1, title:'board1', card_order:'1;2;3'}, {id:2, title:'board2', card_order:'4;5;6'}]";
+        return '[{"id":1, "title":"board1", "card_order":"1;2;3"}, {"id":2, "title":"board2", "card_order":"4;5;6"}]';
     };
 
     this.get_board = function(id) {
-        return "{id:1, title:'board1', card_order:'1;2;3'}";
+        return '{"id":1, "title":"board1", "card_order":"1;2;3"}';
     };
 
     this.get_all_cards = function(table_id) {
-        return "[{id:1, title:'task', description:'this is a good task men', status:'new', order:0}, {id:2, title:'task', description:'this is a good task men', status:'new', order:1}, {id:3, title:'task', description:'this is a good task men', status:'new', order:2}]";
+        return '[{"id":1, "title":"task", "description":"this is a good task men", "status":"new", "order":0}, {"id":2, "title":"task", "description":"this is a good task men", "status":"new", "order":1}, {"id":3, "title":"task", "description":"this is a good task men", "status":"new", "order":2}]';
     };
 
     this.get_card = function(id) {
-        return "{id:1, title:'task', description:'this is a good task men', status:'new', order:0}";
+        return '{"id":1, "title":"task", "description":"this is a good task men", "status":"new", "order":0}';
     };
 }
 
