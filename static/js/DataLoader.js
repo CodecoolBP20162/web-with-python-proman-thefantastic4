47f14439f1cfe99b2475a390ef0f58fe28543931
mankamolnar <markmanomolnar@gmail.com>
2017-03-21 14:44:05 +0100

    get all boards and get exact board finished, get all cards in progress

--- static/js/DataLoader.js
+++ static/js/DataLoader.js
@@ -1,23 +1,50 @@
 function LocalStorageState() {
 
+    // COMMON METHODS
     this.get_all_boards = function() {
         if (localStorage.getItem("boards") === null) {
             return false;
         }
-        return localStorage.getItem("boards");
+
+        return this.get_boards_from_localstorage();
     };
 
-    this.get_board = function(id) {
-        return true;
+    this.get_board = function(board_id) {
+        if (localStorage.getItem("board" + board_id) === null) {
+            return false;
+        }
+
+        return localStorage.getItem("board" + board_id);
     };
 
-    this.get_all_cards = function() {
-        return true;
+    this.get_all_cards = function(board_id) {
+        chosen_board = this.get_board(board_id);
+
+        if (chosen_board !== null) {
+            chosen_board = JSON.parse(chosen_board);
+            return chosen_board.id;
+        }
     };
 
     this.get_card = function(id) {
         return true;
     };
+
+    // ADDITIONAL METHODS
+    this.get_boards_from_localstorage = function() {
+        board_ids = localStorage.getItem("boards").split(";");
+        boards_list_string = "[";
+
+        for (var current_board_index = 0; current_board_index < board_ids.length; current_board_index++) {
+            if (current_board_index == board_ids.length - 1) {
+                boards_list_string += localStorage.getItem("board" + board_ids[current_board_index]) + "]";
+                break
+            }
+            boards_list_string += localStorage.getItem("board" + board_ids[current_board_index]) + ", ";
+        }
+
+        return boards_list_string;
+    }
 }
 
 function PsqlState() {
@@ -60,8 +87,8 @@ function DataLoader(state) {
         return this.state.get_board(id);
     };
 
-    this.get_all_cards = function() {
-        return this.state.get_all_cards();
+    this.get_all_cards = function(board_id) {
+        return this.state.get_all_cards(board_id);
     };
 
     this.get_card = function(id) {
