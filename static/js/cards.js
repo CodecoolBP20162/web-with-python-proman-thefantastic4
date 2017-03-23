50cfdfb1c91e64b731990444aa8ffccf8601416e
szkrecsi <ecsenszki.judit@gmail.com>
2017-03-23 17:29:33 +0100

    Update cards.js

--- static/js/cards.js
+++ static/js/cards.js
@@ -1,56 +1,32 @@
 $(document).ready(function(){
 
     var status_checker = function(all_cards) {
-        var empty_card = $('#1.1');
-        var empty_title = $('#1.1 .title-input');
+        var empty_card = $('#1');
+        var empty_title = $('#1 .title-input'); 
         var empty_description = $('#1 .task');
-        var append_value = $('.sortable');
-        //$('.sortable').html('');
+        $('.sortable').html('');
         for (card in all_cards) {
             if (all_cards[card].status==='new') {
-                alert(all_cards[card].description)
-                empty_title.val(all_cards[card].title);
-                empty_description.val(all_cards[card].description);
-                //empty_card.clone().appendTo('.row');
-                var empty_clone = empty_card.clone()
-                empty_clone.appendTo(append_value);
-
-
-            //     fill_with_cards(all_cards,$('#new-cards > #card > .card-description'),$('#new-cards'));
-            // } else if (all_cards[card].status=='in-progress') {
-            //     fill_with_cards(all_cards,$('#in-progress-cards > .card > .card-description'),$('#in-progress-cards'));
-            // } else if (all_cards[card].status=='review') {
-            //     fill_with_cards(all_cards,$('#review-cards > .card > .card-description'),$('#review-cards'));
-            // } else if (all_cards[card].status=='done') {
-            //     fill_with_cards(all_cards,$('#done-cards > .card > .card-description'),$('#done-cards'));
+                var where = '#new-cards .sortable';
+                fill_with_cards(all_cards, empty_card, empty_title, empty_description, where);       
+            } else if (all_cards[card].status==='in-progress') {
+                var where = '#in-progress-cards .sortable';
+                fill_with_cards(all_cards, empty_card, empty_title, empty_description, where);  
+            } else if (all_cards[card].status==='review') {
+                var where = '#review-cards .sortable';
+                fill_with_cards(all_cards, empty_card, empty_title, empty_description, where);  
+            } else if (all_cards[card].status==='done') {
+                var where = '#done-cards .sortable';
+                fill_with_cards(all_cards, empty_card, empty_title, empty_description, where);
             };
         };
     };
 
-    // var fill_with_cards = function(all_cards) {
-    //     //var empty_card = $('#new-cards > #card ');
-    //     // var empty_title = $('#review-cards> .card-title');
-    //     var empty_card = $('#empty_card');
-    //     var empty_title = $('.title');
-
-    //     var empty_description = card_desc;
-    //     $('.sortable').html(''); //OK
-
-    //     for (card in all_cards) {
-
-    //     };
-    // };
-    // var fill_with_cards = function(all_cards) {
-    //     var empty_card = $('#empty_card');
-    //     var empty_title = $('.title-input');
-    //     var empty_task = $('.task');
-    //     //$('.row').html('');
-    //     for (card in all_cards) {
-    //         empty_title.text(all_cards[card].title);
-    //         empty_task.text(all_cards[card].description);
-    //         empty_card.clone().appendTo('.row');
-    //     };
-    // };
+    var fill_with_cards = function(all_cards, empty_card, empty_title, empty_description, where) {
+        empty_title.html(all_cards[card].title);
+        empty_description.html(all_cards[card].description);
+        empty_card.clone().appendTo(where);
+    }
 
     $('.board-title').click(function(){
         //var data_loader = new DataLoader("localstorage");
@@ -59,14 +35,23 @@ $(document).ready(function(){
         var board = data_loader.get_board(board_html_id);
         board = JSON.parse(board);
         var board_title = board.title;
-
+ 
         $('.board-title').html(board_title);
         $('title').html(board_title);
 
         var all_cards = data_loader.get_all_cards(board.id);
         all_cards = JSON.parse(all_cards);
+        
+        data_loader.set_active_board(board.id);
+        var active_board = data_loader.get_active_board();
 
         //fill_with_cards(all_cards);
         status_checker(all_cards);
     });
+
+    $('#add-button').click(function(){
+        var new_card = data_loader.create_card(active_board);
+        var empty_card = $('#1');
+        empty_card.clone().appendTo('#new-cards .sortable');
+
 });
